d03429d0274e4ae5a2d9538d3687c0451af46600
ldy9037 <ldy9037@naver.com>
2022-11-24 09:47:24 +0900

    feat(lv3): 퍼즐 조각 채우기 풀이 (미완)
    어떻게 풀지 구상은 하였음. 깔끔하게 풀고 싶은데 시간이 걸릴 것 같아 코딩 테스트 이후 다시 풀겠음.

---
+++ lv3/Q12.java
@@ -0,0 +1,78 @@
+package lv3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Q12 {
+    public static void main(String[] args){
+        int[][] game_board = {{1,1,0,0,1,0},{0,0,1,0,1,0},{0,1,1,0,0,1},{1,1,0,1,1,1},{1,0,0,0,1,0},{0,1,1,1,0,0}};
+        int[][] table = {{1,0,0,1,1,0},{1,0,1,0,1,0},{0,1,1,0,1,1},{0,0,1,0,0,0},{1,1,0,1,1,0},{0,1,0,0,0,0}};
+
+        SolutionQ12 solution = new SolutionQ12();
+        consoleOut(solution.solution(game_board, table));
+    }
+
+    private static void consoleOut(int result) {
+        System.out.println(result); // 14
+    }
+}
+
+class SolutionQ12 {
+    public int solution(int[][] game_board, int[][] table) {
+        int answer = -1;
+        List<List<int[]>> emptySpace = new ArrayList<>();
+
+        for (int i = 0; i < game_board.length; i++) {
+            for (int k = 0; k < game_board[i].length; k++) {
+                if (game_board[i][k] == 0) {
+                    emptySpace.add(bfs(game_board, i, k));
+                }
+            }
+        }
+
+        // Spin And Compare...
+
+        return answer;
+    }
+
+    private List<int[]> bfs(int[][] board, int row, int col) {
+        List<int[]> result = new ArrayList<>();
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{row, col});
+        board[row][col] = 0;
+        result.add(new int[]{0, 0});
+
+        int[][] cardinalPoints = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        while (!queue.isEmpty()) {
+            int[] location = queue.poll();
+
+            for (int[] cardinalPoint : cardinalPoints) {
+                int[] nextLocation = {location[0] + cardinalPoint[0], location[1] + cardinalPoint[1]};
+
+                if (canGo(board, nextLocation[0], nextLocation[1])) {
+                    queue.add(nextLocation);
+                    result.add(new int[] {nextLocation[0] - row, nextLocation[1] - col});
+                    board[nextLocation[0]][nextLocation[1]] = 0;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private boolean canGo(int[][] board, int row, int col) {
+        if (row >= 0 && row < board.length && col >= 0 && col < board.length) {
+            if (board[row][col] == 1) {
+                return true;
+            }
+        } 
+
+        return false;
+    }
+}
