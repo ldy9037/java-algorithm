b45ab94e19fa6e5d8aa8cd7fe5497835329dcf3e
emadam <ldy9037@naver.com>
2022-11-25 07:21:26 +0900

    refactor(Q12.java): unused import 제거

--- lv3/Q12.java
+++ lv3/Q12.java
@@ -1,9 +1,6 @@
 package lv3;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
